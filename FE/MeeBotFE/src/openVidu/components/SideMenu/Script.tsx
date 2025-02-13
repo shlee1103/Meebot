f0d1ca532b0723b617dbc3a16f1cc0b04d6bb673
siyun <siyun2072@naver.com>
2025-02-13 21:14:28 +0900

    Feat : 특정한 끝말로 문장 구분(스크립트)

--- FE/MeeBotFE/src/openVidu/components/SideMenu/Script.tsx
+++ FE/MeeBotFE/src/openVidu/components/SideMenu/Script.tsx
@@ -13,7 +13,7 @@ const Script: React.FC<ScriptProps> = ({ conferenceStatus, currentScript, curren
     <>
       {(conferenceStatus === CONFERENCE_STATUS.PRESENTATION_ACTIVE && currentPresenter) && (
         <div className="p-4 overflow-y-auto h-full">
-          <div className="p-3 rounded-lg bg-[#E9E8F3] bg-opacity-30 text-[#FFFFFF] text-sm">
+          <div className="p-3 rounded-lg bg-[#E9E8F3] bg-opacity-30 text-[#FFFFFF] text-sm whitespace-pre-wrap">
             {currentScript}
           </div>
         </div>
