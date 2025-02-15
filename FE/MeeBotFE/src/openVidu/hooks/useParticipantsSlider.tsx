c6d1b2ecdb14bd603b880fb12f9ed4090829adc5
thdgml1103 <thdgml1103@gmail.com>
2025-02-16 01:54:29 +0900

    Refactor: 불필요한 주석 제거

--- FE/MeeBotFE/src/openVidu/hooks/useParticipantsSlider.tsx
+++ FE/MeeBotFE/src/openVidu/hooks/useParticipantsSlider.tsx
@@ -5,12 +5,10 @@ export const useParticipantsSlider = (subscribers: Subscriber[], isMenuOpen: boo
   const [currentSlide, setCurrentSlide] = useState<number>(0);
 
   const handlePrevSlide = () => {
-    console.log("이전 버튼 클릭");
     setCurrentSlide((curr) => Math.max(0, curr - 1));
   };
 
   const handleNextSlide = () => {
-    console.log("다음 버튼 클릭");
     setCurrentSlide((curr) => Math.min(curr + 1, Math.max(0, subscribers.length - (isMenuOpen ? 4 : 6))));
   };
 
