1d6ffa568b652cc0c9884306ddd4e462c7d46c2c
thdgml1103 <thdgml1103@gmail.com>
2025-02-16 00:35:07 +0900

    test1

--- FE/MeeBotFE/src/openVidu/hooks/useParticipantsSlider.tsx
+++ FE/MeeBotFE/src/openVidu/hooks/useParticipantsSlider.tsx
@@ -2,14 +2,16 @@ import { useState } from "react";
 import { Subscriber } from "openvidu-browser";
 
 export const useParticipantsSlider = (subscribers: Subscriber[], isMenuOpen: boolean) => {
-  const [ currentSlide, setCurrentSlide ] = useState<number>(0);
+  const [currentSlide, setCurrentSlide] = useState<number>(0);
 
   const handlePrevSlide = () => {
+    console.log("이전 버튼 클릭");
     setCurrentSlide((curr) => Math.max(0, curr - 1));
   };
 
   const handleNextSlide = () => {
-    setCurrentSlide((curr) => Math.min(curr + 1, Math.max(0, subscribers.length - (isMenuOpen ? 3 : 5))));
+    console.log("다음 버튼 클릭");
+    setCurrentSlide((curr) => Math.min(curr + 1, Math.max(0, subscribers.length - (isMenuOpen ? 4 : 6))));
   };
 
   return {
