2f96a1d635d8c272a892ea072a7cd24d71ac780d
thdgml1103 <thdgml1103@gmail.com>
2025-02-16 01:34:07 +0900

    Fix: 이동할 수 있는 경우에만 버튼 띄우기

--- FE/MeeBotFE/src/openVidu/components/ParticipantsList.tsx
+++ FE/MeeBotFE/src/openVidu/components/ParticipantsList.tsx
@@ -12,12 +12,8 @@ interface ParticipantsListProps {
 const ParticipantsList: React.FC<ParticipantsListProps> = ({ subscribers, currentSlide, isMenuOpen, handlePrevSlide, handleNextSlide }) => {
   return (
     <div className="flex items-center w-full my-[1%]">
-      {((isMenuOpen && subscribers.length > 4) || (!isMenuOpen && subscribers.length > 6)) && (
-        <button
-          className="w-[3%] aspect-square flex items-center justify-center bg-white/20 rounded-full text-white"
-          onClick={handlePrevSlide}
-          // disabled={currentSlide === 0}
-        >
+      {((isMenuOpen && subscribers.length > 4) || (!isMenuOpen && subscribers.length > 6)) && currentSlide !== 0 && (
+        <button className="w-[3%] aspect-square flex items-center justify-center bg-white/20 rounded-full text-white" onClick={handlePrevSlide}>
           ◀
         </button>
       )}
@@ -35,12 +31,8 @@ const ParticipantsList: React.FC<ParticipantsListProps> = ({ subscribers, curren
           </div>
         ))}
       </div>
-      {((isMenuOpen && subscribers.length > 4) || (!isMenuOpen && subscribers.length > 6)) && (
-        <button
-          className="w-[3%] aspect-square flex items-center justify-center bg-white/20 rounded-full text-white"
-          onClick={handleNextSlide}
-          // disabled={currentSlide >= subscribers.length - (isMenuOpen ? 4 : 6)}
-        >
+      {((isMenuOpen && subscribers.length > 4) || (!isMenuOpen && subscribers.length > 6)) && currentSlide < subscribers.length - (isMenuOpen ? 4 : 6) && (
+        <button className="w-[3%] aspect-square flex items-center justify-center bg-white/20 rounded-full text-white" onClick={handleNextSlide}>
           ▶
         </button>
       )}
