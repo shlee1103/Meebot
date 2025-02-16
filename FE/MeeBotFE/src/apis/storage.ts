69dd565dd1a571d1d092a68b33f680ab8ff0f329
Baejihae <qowlgo00@gmail.com>
2025-02-16 17:02:33 +0900

    feat: 보관함 api 추가

--- FE/MeeBotFE/src/apis/storage.ts
+++ FE/MeeBotFE/src/apis/storage.ts
@@ -1,6 +1,7 @@
-import apiClient from "./apiClient"
+import apiClient from "./apiClient";
 
-interface StorageItem {
+export interface StorageItem {
+  roomTitle: string;
   roomCode: string;
   content: string;
   createdAt: string;
@@ -8,12 +9,34 @@ interface StorageItem {
 
 // 보관함 데이터 가져오기
 export const getStorageData = async (): Promise<StorageItem[]> => {
-  const response = await apiClient.get('/api/storage')
-  return response.data
-}
+  const response = await apiClient.get("/api/storage");
+  return response.data;
+};
 
 // 보관함 데이터 삭제
 export const deleteStorageData = async (roomCode: string): Promise<string> => {
-  const response = await apiClient.delete(`/api/storage/${roomCode}`)
-  return response.data
+  const response = await apiClient.delete(`/api/storage/${roomCode}`);
+  return response.data;
+};
+
+// 보관함 데이터 노션으로 다운
+export const saveNotion = async (roomCode: string): Promise<string> => {
+  const response = await apiClient.get(`/api/download/login`, {
+    data: { room_code: roomCode },
+  });
+  return response.data;
+};
+
+// 보관함 데이터 pdf로 다운
+export const savePdf = async (roomCode: string): Promise<string> => {
+  const response = await apiClient.get(`/api/download/pdf`, {
+    data: { room_code: roomCode },
+  });
+  return response.data;
 }
+
+// 최종 요약본 저장
+export const saveSummary = async (roomCode: string): Promise<string> => {
+  const response = await apiClient.post(`/api/chatgpt/final-summarize`, { roomCode: roomCode });
+  return response.data;
+};
